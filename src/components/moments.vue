a067d68a9ee0a16e213d54f75621248f066e0aa5
lqf <1454554357@qq.com>
2018-02-27 17:34:55 +0800

    Create moments.vue

---
+++ src/components/moments.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id='create'>create a comment</div>
+</template>
+
+<script>
+</script>
+
+<style scoped="scoped">
+	#create{
+		width:200px;
+		height:50px;
+		line-height:50px;
+		text-align: center;
+		background: #000;
+		opacity: 0.3;
+		color:#fff;
+		border-radius:8px;
+		cursor: pointer;
+		position: fixed;
+		top:100px;
+		right:50px;
+	}
+	#create:hover{
+		opacity: 0.4;
+	}
+	#create:active{
+		opacity: 0.8;
+	}
+</style>
