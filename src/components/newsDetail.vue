60ae9a3c1fc9e4d33ede2ea7c29ebdb41e386368
lqf <1454554357@qq.com>
2018-02-27 15:00:19 +0800

    Update newsDetail.vue
    传递参数获取详情信息

--- src/components/newsDetail.vue
+++ src/components/newsDetail.vue
@@ -1,12 +1,37 @@
 <template>
 	<div>
-		
+		<div>id is {{msg}}</div>
+		<div>{{detail}}</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		
+		name:"newsdetail",
+		data(){
+			return {
+				id:this.$route.query.id,
+			}
+		},
+		computed:{
+			msg:function(){
+				return this.id
+			},
+			detail:function(){
+		   		return "接口状态："+this.$store.state.newDetail.code
+		   	}
+		},
+		methods:{
+			getNewsDetail:function(){
+				let param={
+					id:this.id
+				};
+				this.$store.dispatch('getNewsDetail',param).code
+			}
+		},
+		created(){
+			this.getNewsDetail();
+		}
 	}
 </script>
 
